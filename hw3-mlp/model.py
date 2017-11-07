b2d52999208cc605dcb5c8a5510865f14293a0f8
ivanium <326698794@qq.com>
2017-11-07 08:33:12 +0800

    init

---
+++ hw3-mlp/model.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+
+
+class Model:
+    def __init__(self,
+                 is_train,
+                 learning_rate=0.001,
+                 learning_rate_decay_factor=0.9995):
+        self.x_ = tf.placeholder(tf.float32, [None, 28*28])
+        self.y_ = tf.placeholder(tf.int32, [None])
+        self.keep_prob = tf.placeholder(tf.float32)
+
+        self.total_mean1 = tf.Variable(tf.constant(0., shape=[100]))
+        self.total_var1  = tf.Variable(tf.constant(0., shape=[100]))
+        self.total_num1  = 0
+
+        # TODO:  implement input -- Linear -- BN -- ReLU -- Linear -- loss
+        X = self.x_
+        W1 = weight_variable([28*28, 100])
+        b1 = bias_variable([100])
+        bnl1 = batch_normalization_layer(tf.matmul(X, W1) + b1, self.total_mean1, 
+            self.total_var1, self.total_num1, is_train)
+        h1 = tf.nn.relu(bnl1)
+        W_l = weight_variable([100, 10])
+        b_l = bias_variable([10])
+        logits = tf.matmul(h1, W_l) + b_l
+        #        the 10-class prediction output is named as "logits"
+        # logits = tf.Variable(tf.constant(0.0, shape=[100, 10]))  # deleted this line after you implement above layers
+
+        self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y_, logits=logits))
+        self.correct_pred = tf.equal(tf.cast(tf.argmax(logits, 1), tf.int32), self.y_)
+        self.pred = tf.argmax(logits, 1)  # Calculate the prediction result
+        self.acc = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))  # Calculate the accuracy in this mini-batch
+
+        self.learning_rate = tf.Variable(float(learning_rate), trainable=False, dtype=tf.float32)
+        self.learning_rate_decay_op = self.learning_rate.assign(self.learning_rate * learning_rate_decay_factor)  # Learning rate decay
+
+        self.global_step = tf.Variable(0, trainable=False)
+        self.params = tf.trainable_variables()
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss, global_step=self.global_step,
+                                                                            var_list=self.params)  # Use Adam Optimizer
+
+        self.saver = tf.train.Saver(tf.global_variables(), write_version=tf.train.SaverDef.V2,
+                                    max_to_keep=3, pad_step_number=True, keep_checkpoint_every_n_hours=1.0)
+
+
+def weight_variable(shape):  # you can use this func to build new variables
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):  # you can use this func to build new variables
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def batch_normalization_layer(inputs, total_mean, total_var, total_num, isTrain=True):
+    # TODO: implemented the batch normalization func and applied it on fully-connected layers
+    epsilon = 0.0001
+    inputs_shape = inputs.get_shape()
+    # inputs_shape[0] = 1
+    length = int(inputs_shape[-1])
+    gamma = weight_variable([length])
+    beta = bias_variable([length])
+    if(isTrain):
+        mean = tf.reduce_mean(inputs, 0) #shape: [1, length]
+        var = tf.reduce_mean(tf.pow(inputs - mean, 2), 0)
+        total_mean = total_mean + mean
+        total_var  = total_var + var
+        total_num  = total_num + 1
+        inputs_normed = (inputs - mean) / tf.sqrt(var + epsilon)
+        return tf.multiply(inputs_normed, gamma) + beta
+    else:
+        mean = total_mean / total_num
+        var = total_var / total_num
+        inputs_normed = (inputs - mean) / tf.sqrt(var + epsilon)
+        return tf.multiply(inputs_normed, gamma) + beta
+
